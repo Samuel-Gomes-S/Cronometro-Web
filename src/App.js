cae64d226e9e75f55d1c30d2d1190d8cb78b43c3
Samuel Gomes <sam2022gs@gmail.com>
2024-10-27 15:43:41 -0300

    App Cronometro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+//Variaveis globais para acompanhar os segundos, minutos, horas
+let seconds = 0
+let minutes = 0
+let hours = 0
+
 function App() {
+
+  const [time, setTime] = useState(0)
+  const [buttonText, setButtonText] = useState('Iniciar')
+  const [timer, setTimer] = useState(null)
+
+  useEffect(() => {
+    return () => clearInterval(timer) // para limpar o intervalo ao desmontar o componente ou quando `timer` muda 
+  }, [timer])
+
+  //função de iniciar o timer
+  function playTime() {
+
+    if (timer) {
+
+      clearInterval(timer)
+      setTimer(null)
+      setButtonText('Iniciar')
+
+    } else {
+
+      setTimer(
+
+        setInterval(() => {
+
+          let timeFormated;
+
+          if (seconds < 60) {
+            seconds++
+          }
+
+          if (seconds === 60) {
+            seconds = 0
+            minutes++
+          }
+
+          if (minutes === 60) {
+            minutes = 0
+            hours++
+          }
+
+          timeFormated = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ":" + (seconds < 10 ? '0' + seconds : seconds)
+          setTime(timeFormated)
+
+        }, 1000)
+      )
+
+      setButtonText('Pausar')
+    }
+  }
+
+  //função para limpar o timer
+  function clearTime() {
+    if (time === 0) return //apenas verificar  se o cronometro já está limpo
+
+    //reseta as variáveis de tempo e atualiza o estado
+    clearInterval(timer)
+    setTimer(null)
+    setButtonText('Iniciar')
+    seconds = 0
+    minutes = 0
+    hours = 0
+    setTime(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='container' >
+      <div id='content'>
+        <p id='crono'> {time} </p>
+        <div id='contentButton'>
+          <button
+            className='buttons'
+            onClick={() => playTime()}
+          >
+            {buttonText}
+          </button>
+          <button
+            className='buttons'
+            onClick={() => clearTime()}
+          >
+            Limpar
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
+
+
+
 
 export default App;
